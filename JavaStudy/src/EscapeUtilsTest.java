a7a12d92562bcb4dcd5ac10d8a0e1f843817c5a2
vg <vg>
2023-05-01 19:56:36 +0530

    Add code files

--- JavaStudy/src/EscapeUtilsTest.java
+++ JavaStudy/src/EscapeUtilsTest.java
@@ -1,16 +1,16 @@
-import org.apache.commons.lang3.StringEscapeUtils;
+//import org.apache.commons.lang3.StringEscapeUtils;
 import org.junit.Test;
 
 public class EscapeUtilsTest {
     @Test
     public void test1() {
-        System.out.println(StringEscapeUtils.escapeJava("ab''c"));
-        System.out.println(StringEscapeUtils.escapeJava ("ab''c"));
-        System.out.println(StringEscapeUtils.escapeJava("ab\"c"));
+//        System.out.println(StringEscapeUtils.escapeJava("ab''c"));
+//        System.out.println(StringEscapeUtils.escapeJava ("ab''c"));
+//        System.out.println(StringEscapeUtils.escapeJava("ab\"c"));
         System.out.println("ab\"c");
-        System.out.println(StringEscapeUtils.escapeJava("ab\tc"));
-        System.out.println(StringEscapeUtils.escapeJava("ab\tc"));
-        System.out.println(StringEscapeUtils.escapeJava("ab\\\"c"));
+//        System.out.println(StringEscapeUtils.escapeJava("ab\tc"));
+//        System.out.println(StringEscapeUtils.escapeJava("ab\tc"));
+//        System.out.println(StringEscapeUtils.escapeJava("ab\\\"c"));
     }
 }
 
---
+++ KotlinStudy/src/ArrayTest.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val message = "Hello World!"
+    println(message)
+
+    var num = 1..100
+}
